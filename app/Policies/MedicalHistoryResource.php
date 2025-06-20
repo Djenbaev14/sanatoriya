12546a78446a3c6e7039cbfc1b8288bfd31e2ee2
Djenbaev14 <sjzgtf14@gmail.com>
2025-06-20 15:46:38 +0500

    Remove BedAndMealPolicy and add MedicalHistoryResource for managing access to medical history procedures

--- app/Policies/MedicalHistoryResource.php
+++ app/Policies/MedicalHistoryResource.php
@@ -6,21 +6,20 @@ use App\Models\MedicalHistory;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class BedAndMealPolicy
+class MedicalHistoryResource
 {
     use HandlesAuthorization;
     public function viewAny(User $user): bool
     {
-        return $user->can('просмотреть любую касса процедуру');
+        return $user->can('просмотреть любую касса питание и койка');
     }
-
     public function view(User $user, MedicalHistory $medicalHistory): bool
     {
-        return $user->can('просмотреть касса процедуру');
+        return $user->can('просмотреть касса питание и койка');
     }
 
     public function create(User $user): bool
     {
-        return $user->can('создать касса процедуру');
+        return $user->can('создать касса питание и койка');
     }
 }
