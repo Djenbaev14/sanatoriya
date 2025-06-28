d6c0d52782bc545c604e475f60ea4d787e3cff28
Djenbaev14 <sjzgtf14@gmail.com>
2025-06-28 15:23:42 +0500

    Add ViewDepartmentInspection page and update inspection link in department inspection view

---
+++ app/Filament/Resources/DepartmentInspectionResource/Pages/ViewDepartmentInspection.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\DepartmentInspectionResource\Pages;
+
+use App\Filament\Resources\DepartmentInspectionResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewDepartmentInspection extends ViewRecord
+{
+    protected static string $resource = DepartmentInspectionResource::class;
+}
