2de8412d515a360dae49fe4f555b90054d8a1244
Djenbaev14 <sjzgtf14@gmail.com>
2025-07-02 11:56:37 +0500

    fix: Require 'is_accomplice' field in PatientResource form and restore CreateAction visibility in ListPatients page

--- app/Filament/Resources/PatientResource/Pages/ListPatients.php
+++ app/Filament/Resources/PatientResource/Pages/ListPatients.php
@@ -9,5 +9,10 @@ use Filament\Resources\Pages\ListRecords;
 class ListPatients extends ListRecords
 {
     protected static string $resource = PatientResource::class;
-
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make()->visible(fn () => auth()->user()->can('создать больной')),
+        ];
+    }
 }
