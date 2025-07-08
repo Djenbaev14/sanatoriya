998776a788982cddb20fddf47ac550e73704f874
Djenbaev14 <sjzgtf14@gmail.com>
2025-07-08 11:08:59 +0500

    feat: Ensure 'id' is guarded in Tariff model for security

--- app/Models/Tariff.php
+++ app/Models/Tariff.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tariff extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
     public function wards()
     {
         return $this->hasMany(Ward::class);
