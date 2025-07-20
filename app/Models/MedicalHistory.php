9c4c99f331ac49d9ac13d5e7c217e0f62e5ed432
Djenbaev14 <sjzgtf14@gmail.com>
2025-07-20 14:16:15 +0500

    fafa

--- app/Models/MedicalHistory.php
+++ app/Models/MedicalHistory.php
@@ -119,12 +119,12 @@ class MedicalHistory extends Model
     }
     public function getUnpaidPartnerWardDays()
     {
-        $paid = AccommodationPayment::where('accommodation_id','=',$this?->partnerAccommodation->id)->sum('ward_day');
+        $paid = AccommodationPayment::where('accommodation_id','=',$this?->partnerAccommodation?->id)->sum('ward_day');
         return max(0, $this->partnerAccommodation?->ward_day - $paid);
     }
     public function getUnpaidPartnerMealDays()
     {
-        $paid = AccommodationPayment::where('accommodation_id','=',$this?->partnerAccommodation->id)->sum('meal_day');
+        $paid = AccommodationPayment::where('accommodation_id','=',$this?->partnerAccommodation?->id)->sum('meal_day');
         return max(0, $this->partnerAccommodation?->meal_day - $paid);
     }
     
