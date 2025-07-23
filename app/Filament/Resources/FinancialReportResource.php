6bff6b97d492d3d2b1755d43519ffc8283cd4db7
Djenbaev14 <sjzgtf14@gmail.com>
2025-07-23 10:35:00 +0500

    afaf

--- app/Filament/Resources/FinancialReportResource.php
+++ app/Filament/Resources/FinancialReportResource.php
@@ -83,7 +83,7 @@ class FinancialReportResource extends Resource
                     ->exports([
                          ExcelExport::make()
                         ->modifyQueryUsing(function ($query, $livewire) {
-                            return $livewire->getFilteredTableQuery(); // Filtrlangan queryni qaytaradi
+                            return $livewire->getFilteredTableQuery()->orderBy('number','desc'); // Filtrlangan queryni qaytaradi
                         })
                          ->withColumns([
                             Column::make('№') // tartib raqami
