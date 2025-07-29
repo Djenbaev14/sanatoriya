d023588ac628eac03bc32c0d5fc0e54bdd0b525b
Djenbaev14 <sjzgtf14@gmail.com>
2025-07-29 11:05:40 +0500

    dafa

--- resources/views/filament/components/ward-summary.blade.php
+++ resources/views/filament/components/ward-summary.blade.php
@@ -1,5 +1,5 @@
-<div class="px-4 py-2 bg-gray-100 rounded-lg flex flex-wrap gap-4 text-sm font-medium">
-    <div><strong>Всего палаты:</strong> {{ $wardsCount }}</div><hr>
+<div class="px-4 py-2 bg-gray-100 rounded-lg text-lg font-medium">
+    <div><strong>Всего палаты:</strong> {{ $wardsCount }}</div>
     <div><strong>Всего месты:</strong> {{ $totalPlaces }}</div>
     <div><strong>Занято:</strong> {{ $busyBeds }}</div>
     <div><strong>Свободно:</strong> {{ $freeBeds }}</div>
