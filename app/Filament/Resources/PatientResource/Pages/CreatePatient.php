f4a131d7a1d41caa22045ce1bc261abd4be727a0
Djenbaev14 <sjzgtf14@gmail.com>
2025-09-08 12:01:03 +0500

    aeffea

--- app/Filament/Resources/PatientResource/Pages/CreatePatient.php
+++ app/Filament/Resources/PatientResource/Pages/CreatePatient.php
@@ -13,4 +13,25 @@ class CreatePatient extends CreateRecord
     {
         abort_unless(auth()->user()->can('создать больной'), 403);
     }
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        // Masalan, auth user qo‘shish
+        $data['user_id'] = auth()->id();
+
+        // JSON field formatlash
+        if (isset($data['photo']) && str_starts_with($data['photo'], 'data:image')) {
+            $data['photo'] = $this->saveBase64Image($data['photo']);
+        }
+
+        return $data;
+    }
+
+    private function saveBase64Image(string $base64): string
+    {
+        $image = str_replace('data:image/png;base64,', '', $base64);
+        $image = str_replace(' ', '+', $image);
+        $fileName = 'patients/' . uniqid() . '.png';
+        \Storage::disk('public')->put($fileName, base64_decode($image));
+        return $fileName;
+    }
 }
